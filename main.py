37b4d7007de2c451697a664f4306d1c359c9bef6
zahrag <zahra.gharaee@gmail.com>
2023-07-06 14:21:47 -0400

    fix crop tool

--- main.py
+++ main.py
@@ -61,7 +61,7 @@ if __name__ == '__main__':
 
     # ################################# RESIZE IMAGES ##############################################
     make_resize(configs["image_path"], configs["resized_image_path"],  configs['resized_hdf5_path'],
-                saved_as_binary_data=True, resize_dimension=256, zip_name="original_256")
+                saved_as_binary_data=True, resize_dimension=256, zip_name="original_256.zip")
 
     # ##################################### RUN PRE-PROCESSING ###################################################
     run_crop_tool(configs)
