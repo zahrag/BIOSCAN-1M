07f735dd2c55d7f6d56c3219a8b8def5f8f27306
zahrag <zahra.gharaee@gmail.com>
2023-07-05 23:52:08 -0400

    fix crop tool

--- resize_image.py
+++ resize_image.py
@@ -2,9 +2,10 @@ import os
 from utils import resize_image, make_directory
 import h5py
 import numpy as np
+from PIL import Image, UnidentifiedImageError
 
 
-def make_resize(configs):
+def make_resize(configs, saved_as_binary_data=True):
 
     if configs["resized_image_path"] is not None:
         full_size_img_path = configs["image_path"]
@@ -14,8 +15,22 @@ def make_resize(configs):
 
         if configs['resized_hdf5_path'] is not None:
             with h5py.File(configs['resized_hdf5_path'], 'w') as hdf5:
+
                 for img in os.listdir(resized_img_path):
-                    with open(f"{resized_img_path}/{img}", 'rb') as img_f:
-                        binary_data = img_f.read()
-                    binary_data_np = np.asarray(binary_data)
-                    hdf5.create_dataset(f'{img}', data=binary_data_np)
+                    image_dir = f"{resized_img_path}/{img}"
+
+                    if saved_as_binary_data:
+                        with open(image_dir, 'rb') as img_f:
+                            binary_data = img_f.read()
+                        binary_data_np = np.asarray(binary_data)
+                        hdf5.create_dataset(f'{img}', data=binary_data_np)
+                    else:
+
+                        try:
+                            image = Image.open(image_dir)
+                            image_array = np.array(image)
+                            hdf5.create_dataset(f'{img}', data=image_array)
+
+                        except UnidentifiedImageError:
+                            print(f"{img} Corrupted.")
+                            continue
