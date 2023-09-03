dea320fda910e0ddb5258f8ba667354f6f9f874a
zahrag <zahra.gharaee@gmail.com>
2023-09-03 16:35:18 -0400

    add instructions for crop-tool checkpoint

--- generalization.py
+++ generalization.py
@@ -142,7 +142,8 @@ if __name__ == '__main__':
     """
     This script is written to predict Order labels of new images taken from Insects by Centre for Biodiversity Genomics.
     Additionally the code allows to evaluate generalization capabilities of our best model trained on the cropped and 
-    resized BIOSCAN-1M-Insect Large dataset images accessible online via zenodo:https://zenodo.org/record/8030065.
+    resized BIOSCAN-1M-Insect Large dataset images accessible online through project's GoogleDrive 
+    and zenodo:https://zenodo.org/record/8030065.
     
     To run the code, please assure correct path settings to the directory where the new images (image_path) as well as 
     the model checkpoint (model_path) are saved.
